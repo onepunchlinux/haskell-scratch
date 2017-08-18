6a0de66c14a7fef6bbb9511dea05c2b8c1b6ce5e
Michael Whitehead <mwhitehead@bigsquid.com>
2017-08-18 12:56:03 -0600

    adds more libs for postgres, odbc, and crypto

--- Makefile
+++ Makefile
@@ -5,15 +5,15 @@ integer-gmp: | clean
 	@sudo chown -R $(USER) root
 
 docker-integer-gmp: | integer-gmp
-	@tar -cC root .|docker import - haskell-scratch:integer-gmp-libstdc-libgcc
+	@tar -cC root .|docker import - haskell-scratch:all
 
 clean:
 	@rm -rf root
 
 push:
-	@docker tag haskell-scratch:integer-gmp-libstdc-libgcc whitehead1415/haskell-scratch:integer-gmp-libstdc-libgcc
-	@docker push whitehead1415/haskell-scratch:integer-gmp-libstdc-libgcc
-	@docker tag haskell-scratch:integer-gmp-libstdc-libgcc whitehead1415/haskell-scratch:latest
+	@docker tag haskell-scratch:all whitehead1415/haskell-scratch:all
+	@docker push whitehead1415/haskell-scratch:all
+	@docker tag haskell-scratch:all whitehead1415/haskell-scratch:latest
 	@docker push whitehead1415/haskell-scratch:latest
 
 .PHONY: default docker-integer-gmp docker-integer-simple clean push
--- copy-files.sh
+++ copy-files.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+apt-get -y update
+apt-get -y install unixodbc
+
 mkdir root
 mkdir root/bin
 mkdir root/etc
@@ -17,10 +20,23 @@ cp -L /lib/x86_64-linux-gnu/libnss_files.so.2 root/lib/x86_64-linux-gnu/
 cp -L /lib/x86_64-linux-gnu/libnss_dns.so.2 root/lib/x86_64-linux-gnu/
 cp -L /lib/x86_64-linux-gnu/libresolv.so.2 root/lib/x86_64-linux-gnu/
 cp -L /lib/x86_64-linux-gnu/libgcc_s.so.1 root/lib/x86_64-linux-gnu/
+cp -L /lib/x86_64-linux-gnu/libpcre.so.3 root/lib/x86_64-linux-gnu/
+cp -L /lib/x86_64-linux-gnu/libpcre.so.3 root/lib/x86_64-linux-gnu/
+cp -L root/lib/x86_64-linux-gnu/libpcre.so.3 root/lib/x86_64-linux-gnu/libpcre.so.1
+cp -L /lib/x86_64-linux-gnu/libssl.so.1.0.0 root/lib/x86_64-linux-gnu/
+cp -L /lib/x86_64-linux-gnu/libcrypto.so.1.0.0 root/lib/x86_64-linux-gnu/
+cp -L /lib/x86_64-linux-gnu/libcom_err.so.2 root/lib/x86_64-linux-gnu/
+cp -L /lib/x86_64-linux-gnu/libkeyutils.so.1 root/lib/x86_64-linux-gnu/
+cp -L /lib/x86_64-linux-gnu/libcrypt.so.1 root/lib/x86_64-linux-gnu/
+
 mkdir root/lib64
 cp -L /lib64/ld-linux-x86-64.so.2 root/lib64/
 cp -L /etc/protocols root/etc/
 cp -L /etc/services root/etc/
+
+mkdir -p root/etc/ssl/certs
+cp -L /etc/ssl/certs/ca-certificates.crt root/etc/ssl/certs/
+
 mkdir root/usr
 mkdir root/usr/lib
 mkdir root/usr/lib/x86_64-linux-gnu
@@ -32,3 +48,30 @@ cp -L /usr/lib/x86_64-linux-gnu/gconv/gconv-modules root/usr/lib/x86_64-linux-gn
 cp -L /usr/lib/x86_64-linux-gnu/gconv/gconv-modules.cache root/usr/lib/x86_64-linux-gnu/gconv/
 cp -L /usr/lib/x86_64-linux-gnu/libgmp.so.10 root/usr/lib/x86_64-linux-gnu/
 cp -L /usr/lib/x86_64-linux-gnu/libstdc++.so.6 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libpq.so.5 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libodbc.so.2 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libltdl.so.7 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libkrb5.so.3 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libk5crypto.so.3 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libkrb5support.so.0 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libldap_r-2.4.so.2 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/liblber-2.4.so.2 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libsasl2.so.2 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libgssapi_krb5.so.2 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libgssapi.so.3 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libgnutls.so.30 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libheimntlm.so.0 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libkrb5.so.26 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libasn1.so.8 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libhcrypto.so.4 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libroken.so.18 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libp11-kit.so.0 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libidn.so.11 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libtasn1.so.6 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libnettle.so.6 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libhogweed.so.4 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libwind.so.0 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libheimbase.so.1 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libhx509.so.5 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libsqlite3.so.0 root/usr/lib/x86_64-linux-gnu/
+cp -L /usr/lib/x86_64-linux-gnu/libffi.so.6 root/usr/lib/x86_64-linux-gnu/
